c265a30a1d415d44157ed533d4c5d3fa2fa9927e
Ganesh Rajput <ganeshrajput7045@gmail.com>
2024-11-26 23:38:30 +0530

    Update app.py
    Issues in the Code is 
    1)Conflicting imports:
    
    "from urllib.parse import urlencode" and "from urllib import urlencode ": The second import is invalid in Python 3 because "urlencode" is part of "urllib.parse", not "urllib".
    
    Similarly, "from urllib.request import urlopen" and "from urllib2 import urlopen" : In Python 3," urllib2" has been replaced by "urllib.request".
    
    2)Improper decoding:
    
    "response.read().decode('utf-8 ')" has an extra space in 'utf-8 '. It should be 'utf-8'.
    Context manager (with) misunderstanding:
    
    contextlib.closing isn't necessary since urlopen in Python 3 is already a context manager.
    
    
    
    Removed duplicate imports:
    
    Used only urlencode from urllib.parse and urlopen from urllib.request.
    Fixed decoding error:
    
    Removed the space in 'utf-8 ' to avoid runtime errors.
    Simplified resource handling:
    
    Directly used with urlopen(...) since urlopen supports the context manager.
    Improved usability:
    
    Added a usage message if no URLs are provided as command-line arguments.

--- URL_SHORTENER/app.py
+++ URL_SHORTENER/app.py
@@ -1,19 +1,18 @@
-from __future__ import with_statement                                                            
-import contextlib 
-from urllib.parse import urlencode           
-from urllib import urlencode 
-from urllib.request import urlopen 
-from urllib2 import urlopen 
-import sys 
-  
-def short_url(url): 
-    request_url = ('http://tinyurl.com/api-create.php?' + urlencode({'url':url}))     
-    with contextlib.closing(urlopen(request_url)) as response:                       
-        return response.read().decode('utf-8 ')                                       
-  
-def main():                                                                 
-    for url in map(short_url, sys.argv[1:]):                    
-        print(url) 
-  
-if __name__ == '__main__': 
-    main() 
+from urllib.parse import urlencode
+from urllib.request import urlopen
+import sys
+
+def short_url(url):
+    request_url = f'http://tinyurl.com/api-create.php?{urlencode({"url": url})}'
+    with urlopen(request_url) as response:
+        return response.read().decode('utf-8')
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <url1> <url2> ...")
+        return
+    for url in sys.argv[1:]:
+        print(short_url(url))
+
+if __name__ == '__main__':
+    main()
